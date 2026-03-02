d197a02db3c794dd83d75917ef2cde9c06df0fc7
Bhavya <bhavyasrilinganagunta@gmail.com>
2026-03-01 20:53:27 -0500

    Update data_ingestion.py

--- src/data_ingestion/data_ingestion.py
+++ src/data_ingestion/data_ingestion.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import logging
+import os
 
+
+class DataIngestion:
+    def __init__(self, data_path: str):
+        self.data_path = data_path
+
+    def load_data(self) -> pd.DataFrame:
+        logging.info("Loading dataset...")
+        df = pd.read_csv(self.data_path)
+        logging.info("Dataset loaded successfully.")
+        return df
