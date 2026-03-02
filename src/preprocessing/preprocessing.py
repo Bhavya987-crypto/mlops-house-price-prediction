57ecb51e0d536fe5608c4d0c282131f44182a23e
Bhavya <bhavyasrilinganagunta@gmail.com>
2026-03-01 20:53:55 -0500

    Update preprocessing.py

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import logging
 
+
+class DataPreprocessing:
+    def clean_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        logging.info("Starting data preprocessing...")
+
+        df = df.dropna()
+
+        logging.info("Data preprocessing completed.")
+        return df
